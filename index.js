d51ffa5360a5fe1e22606f1d5e22d6359993bfa7
kolamat <matanmidupe004@gmail.com>
2023-12-13 19:14:31 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
 // greetingElement.textContent = "Happy__Birthday_Chidinma_Love";
 
 const typed = new Typed(".pray", {
-  strings: ["", "", ""],
+  strings: [
+    "Happy_Birthday_To_You",
+    "Long_Life_And_Prosperity",
+    "More_Money_In_Your_Account",
+    "More_Cake_To_Burst_And_Eat",
+    "Lastly_Love_You",
+  ],
   shuffle: true,
   cursorChar: ".",
   startDelay: 2000,
