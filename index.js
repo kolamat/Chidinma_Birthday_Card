63251915233102734a6abbfe58732003a6babd8a
kolamat <matanmidupe004@gmail.com>
2023-12-13 18:55:55 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Get the greeting element
+var greetingElement = document.getElementById("greeting");
+
+// Update the content of the greeting element
+greetingElement.textContent = "Happy Birthday!";
