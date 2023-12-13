b51480897ac2b2d439370b3e352b0edc655e6965
kolamat <matanmidupe004@gmail.com>
2023-12-13 21:36:12 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.html

--- index.js
+++ index.js
@@ -19,3 +19,20 @@
 // const typed = Typed("#pray", {
 //   String: ["developer"],
 // });
+
+const typed = new Typed("#pray", {
+  strings: [
+    "Happy_Birthday_To_You",
+    "Long_Life_And_Prosperity",
+    "More_Money_In_Your_Account",
+    "More_Cake_To_Burst_And_Eat",
+    "Lastly_Love_You",
+  ],
+  shuffle: false,
+  cursorChar: ".",
+  startDelay: 2000,
+  typeSpeed: 130,
+  backSpeed: 130,
+  backDelay: 2000,
+  loop: true,
+});
