b869245684a39bccdd70cfb9c113d75f8d1eae34
kolamat <matanmidupe004@gmail.com>
2023-12-14 14:30:10 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ let bdg = document.getElementById("image");
 function toggle_bdg() {
   let img = document.createElement("img");
   img.src = "Screenshot 2023-12-13 232218.png";
-  kola.src = "kolade_head.jpg";
   document.getElementById("body").appendChild(img);
   bdg.innerHTML = "Look At yourseif Pretty Soul.";
 }
