d01270778310c7b65b51eb0b60152c93762b2fe8
kolamat <matanmidupe004@gmail.com>
2023-12-14 15:57:48 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ function toggle_bdg() {
   bdg.innerHTML = "Look At yourseif Pretty Soul.";
 }
 
-let res = document.getElementById("result");
+let image1 = document.getElementById("image1");
 
 function toggle_bdg() {
   let img = new Image();
   img.src = "kolade_head.jpg";
   document.getElementById("body").appendChild(img);
-  res.innerHTML = "Image Element Added.";
+  image1.innerHTML = "Image Element Added.";
 }
