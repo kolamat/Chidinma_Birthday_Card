265ed1a3f7902ae2cd38408c89725705d6435511
kolamat <matanmidupe004@gmail.com>
2023-12-18 22:09:51 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -47,3 +47,23 @@ function toggle_bdg3() {
   img.src = "chi5.jpg";
   document.getElementById("body").appendChild(img);
 }
+
+anime({
+  targets: ".anime",
+  translateX: {
+    value: 250,
+    duration: 800,
+  },
+  rotate: {
+    value: 360,
+    duration: 1800,
+    easing: "easeInOutSine",
+  },
+  scale: {
+    value: 2,
+    duration: 1600,
+    delay: 800,
+    easing: "easeInOutQuart",
+  },
+  delay: 250, // All properties except 'scale' inherit 250ms delay
+});
