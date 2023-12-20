9a4060aa2ecea75596939992f6d81db55af59294
kolamat <matanmidupe004@gmail.com>
2023-12-20 11:07:24 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -118,3 +118,17 @@ anime({
 //   easing: "easeOutElastic(1, .8)",
 //   //   loop: true,
 // });
+
+anime({
+  targets: ".img3",
+  keyframes: [
+    { translateY: -40 },
+    { translateX: 250 },
+    { translateY: 40 },
+    { translateX: 0 },
+    { translateY: 0 },
+  ],
+  duration: 4000,
+  easing: "easeOutElastic(1, .8)",
+  loop: true,
+});
