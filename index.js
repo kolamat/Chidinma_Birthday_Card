aaa82514a092a817be28a6584d90c4bcbeb4ba21
kolamat <matanmidupe004@gmail.com>
2023-12-22 08:41:27 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -93,34 +93,6 @@ anime({
   loop: 2,
 });
 
-// anime({
-//   targets: ".img3",
-//   translateX: [
-//     { value: 120, duration: 1000, delay: 500 },
-//     { value: 0, duration: 1000, delay: 500 },
-//   ],
-//   translateY: [
-//     { value: -40, duration: 500 },
-//     { value: 40, duration: 500, delay: 1000 },
-//     { value: 0, duration: 500, delay: 1000 },
-//   ],
-//   scaleX: [
-//     { value: 4, duration: 100, delay: 500, easing: "easeOutExpo" },
-//     { value: 1, duration: 900 },
-//     { value: 4, duration: 100, delay: 500, easing: "easeOutExpo" },
-//     { value: 1, duration: 900 },
-//   ],
-//   scaleY: [
-//     { value: [1.75, 1], duration: 500 },
-//     { value: 2, duration: 50, delay: 1000, easing: "easeOutExpo" },
-//     { value: 1, duration: 450 },
-//     { value: 1.75, duration: 50, delay: 1000, easing: "easeOutExpo" },
-//     { value: 1, duration: 450 },
-//   ],
-//   easing: "easeOutElastic(1, .8)",
-//   loop: true,
-// });
-
 anime({
   targets: ".img3",
   keyframes: [
