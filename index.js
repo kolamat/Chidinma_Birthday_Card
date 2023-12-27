85765536168b1362c6afbd06d907042316bcd259
kolamat <matanmidupe004@gmail.com>
2023-12-27 18:42:32 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js
    	modified:   style.css

--- index.js
+++ index.js
@@ -59,19 +59,6 @@ function toggle_bdg5() {
   bdg.innerHTML = "Look At yourseif Pretty Soul.";
 }
 
-// let main = document.getElementById("body");
-// let display = 0;
-
-// function clear() {
-//   if (display == 1) {
-//     div.style.display = "flex";
-//     display = 0;
-//   } else {
-//     div.style.display = "none";
-//     display = 1;
-//   }
-// }
-
 anime({
   targets: ".img1",
   translate: {
