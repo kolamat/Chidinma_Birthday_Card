bd95f27b55202d1812ff826aab264eaab6873c56
kolamat <matanmidupe004@gmail.com>
2023-12-30 12:10:57 +0100

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.html
    	modified:   index.js
    	modified:   style.css

--- index.js
+++ index.js
@@ -117,8 +117,10 @@ anime({
 
 anime({
   targets: ".rate",
-  width: "100%",
-  easing: "easeInOutQuad",
-  direction: "alternate",
+  scale: [
+    { value: 0.1, easing: "easeOutSine", duration: 500 },
+    { value: 1, easing: "easeInOutQuad", duration: 1200 },
+  ],
+  delay: anime.stagger(200, { grid: [14, 5], from: "center" }),
   loop: true,
 });
